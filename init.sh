b261e95c16b662d9d6c3d68915f5a6ab0ce2158d
Danu <47096585+dpasnizer@users.noreply.github.com>
2023-06-05 21:56:51 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Usage: ./clash [option]"
+echo " proxy     Fetch proxy provider file(s)"
+echo " rule      Fetch rule provider file(s)"
+echo " update    Update this script to newest version"
+
 URL="https://raw.githubusercontent.com/dpasnizer/clashconfig/main"
 
 # rule provier
