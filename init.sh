add46ddb127283f4d3b0c0a968a1ba8f1b0822eb
Danu <47096585+dpasnizer@users.noreply.github.com>
2023-06-05 21:28:14 +0700

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+URL="https://raw.githubusercontent.com/dpasnizer/clashconfig/main"
+# rule provier
+echo "${URL}/rule_provider/adult.yaml"
+echo "${URL}/rule_provider/direct.yaml"
+echo "${URL}/rule_provider/facebook.yaml"
+echo "${URL}/rule_provider/games.yaml"
+echo "${URL}/rule_provider/instagram.yaml"
+echo "${URL}/rule_provider/reject.yaml"
+echo "${URL}/rule_provider/tiktok.yaml"
+echo "${URL}/rule_provider/twitter.yaml"
+echo "${URL}/rule_provider/whatsapp.yaml"
+echo "${URL}/rule_provider/youtube.yaml"
+
+# proxy provider
+echo "${URL}/proxy_provider/proxies.yaml"
