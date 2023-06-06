d10a3e6b007715a96793489a023b53fb46da9bb6
Danu <47096585+dpasnizer@users.noreply.github.com>
2023-06-06 18:32:14 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 clear
-echo "Usage: ./clash [option]"
-echo " - all       : Fetch all provider"
-echo " - proxy     : Fetch proxy provider"
-echo " - rule      : Fetch rule provider"
-echo " - update    : Update this script to newest version"
-echo ""
+
+URL="https://raw.githubusercontent.com/dpasnizer/clashconfig/main"
+COMMAND="all, proxy, rule, update"
+
+if [ ! -n "$1" ]; then
+  echo ""
+  echo "Usage: ./clash [option]"
+  echo " - all       : Fetch all provider"
+  echo " - proxy     : Fetch proxy provider"
+  echo " - rule      : Fetch rule provider"
+  echo " - update    : Update this script to newest version"
+  echo ""
+  exit 1
+fi
 
 get () {
   echo -ne " - ...\r"
@@ -25,25 +33,34 @@ get () {
   fi
 }
 
-URL="https://raw.githubusercontent.com/dpasnizer/clashconfig/main"
 
-# rule provider
-echo "Fetching rule provider.."
-get adult.yaml "${URL}/rule_provider/adult.yaml"
-get direct.yaml "${URL}/rule_provider/direct.yaml"
-get facebook.yaml "${URL}/rule_provider/facebook.yaml"
-get games.yaml "${URL}/rule_provider/games.yaml"
-get instagram.yaml "${URL}/rule_provider/instagram.yaml"
-get reject.yaml "${URL}/rule_provider/reject.yaml"
-get tiktok.yaml "${URL}/rule_provider/tiktok.yaml"
-get twitter.yaml "${URL}/rule_provider/twitter.yaml"
-get whatsapp.yaml "${URL}/rule_provider/whatsapp.yaml"
-get youtube.yaml "${URL}/rule_provider/youtube.yaml"
-echo ""
+if [ "$1" = "rule" ] || [ "$1" = "all" ]; then
+  # rule provider
+  echo "Fetching rule provider.."
+  get adult.yaml "${URL}/rule_provider/adult.yaml"
+  get direct.yaml "${URL}/rule_provider/direct.yaml"
+  get facebook.yaml "${URL}/rule_provider/facebook.yaml"
+  get games.yaml "${URL}/rule_provider/games.yaml"
+  get instagram.yaml "${URL}/rule_provider/instagram.yaml"
+  get reject.yaml "${URL}/rule_provider/reject.yaml"
+  get tiktok.yaml "${URL}/rule_provider/tiktok.yaml"
+  get twitter.yaml "${URL}/rule_provider/twitter.yaml"
+  get whatsapp.yaml "${URL}/rule_provider/whatsapp.yaml"
+  get youtube.yaml "${URL}/rule_provider/youtube.yaml"
+  echo ""
+fi
 
-# proxy provider
-echo "Fetching proxy provider.."
-get proxies.yaml "${URL}/proxy_provider/proxies.yaml"
-echo ""
+if [ "$1" = "proxy" ] || [ "$1" = "all" ]; then
+  # proxy provider
+  echo "Fetching proxy provider.."
+  get proxies.yaml "${URL}/proxy_provider/proxies.yaml"
+  echo ""
+fi
+
+if [[ "$COMMAND" != *"$1"* ]]; then
+  echo "ERROR: $1 is an invalid option"
+  echo ""
+fi
 
 echo "DONE!"
+exit 0
